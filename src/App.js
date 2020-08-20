a94f484928c7ebd6b6fdb1a3696c5bbabaa2edae
hanif aliffudin <hanifaliffudin@gmail.com>
2020-08-20 21:51:27 +0700

    tes

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Content from "./component/Content";
+import ButtonLike from "./component/ButtonLike";
+function App() {}
+
+export default App;
